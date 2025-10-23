b354fe4c0cc9d951bee9c98d60ac7c041bc1a72d
Scottd28 <ar0613@westminsteru.edu>
2025-10-22 22:30:53 -0600

    add updated lab 7

--- labs/lab7/puzzle.py
+++ labs/lab7/puzzle.py
@@ -4,6 +4,8 @@ import argparse
 
 import draw
 
+
+
 # class of board
 # every board is an object of the class Board 
 class Board:
@@ -40,55 +42,76 @@ class Puzzle:
         # call draw.draw_one_tromino(type, board) to draw one type of tromino at the center of the board. The type of the tromino is an integer 1 to 4 as explained in the instruction and the board is an object of Board class where you want to draw the tromino at its center. 
 
         left, right, bottom, top = board.get_boundary()
+        if (right - left + 1) == 2 and (top - bottom + 1) == 2:
+            type = self.get_tromino_type(block, board)
+            draw.draw_one_tromino(type, board)
+            return
+        else:
 
-        mid_x = right//2
-        mid_y = top//2
-        # Quadrant 1 (top-right)
-        q1 = Board(mid_x + 1, right, mid_y + 1, top)
+            mid_x = (left + right) // 2
+            mid_y = (bottom + top) // 2
 
-        # Quadrant 2 (top-left)
-        q2 = Board(left, mid_x, mid_y + 1, top)
+            # Quadrant 1 (top-right)
+            q1 = Board(mid_x + 1, right, mid_y + 1, top)
+            # Quadrant 2 (top-left)
+            q2 = Board(left, mid_x, mid_y + 1, top)
+            # Quadrant 3 (bottom-left)
+            q3 = Board(left, mid_x, bottom, mid_y)
+            # Quadrant 4 (bottom-right)
+            q4 = Board(mid_x + 1, right, bottom, mid_y)
 
-        # Quadrant 3 (bottom-left)
-        q3 = Board(left, mid_x, bottom, mid_y)
+            # your code goes here:
+            typeOriginal = self.get_tromino_type(block, board)
+            draw.draw_one_tromino(typeOriginal, board)
 
-        # Quadrant 4 (bottom-right)
-        q4 = Board(mid_x + 1, right, bottom, mid_y)
+            # After determining typeOriginal for the center tromino:
 
-        if block in q1:
+            if typeOriginal == 1:  # original block in Quadrant 1 (top-right)
+                b1 = block  # Quadrant 1: original block stays
+                b2 = (mid_x, mid_y + 1)  # Quadrant 2: center tromino covers this
+                b3 = (mid_x, mid_y)  # Quadrant 3: center tromino covers this
+                b4 = (mid_x + 1, mid_y)  # Quadrant 4: center tromino covers this
 
-            draw.draw_one_tromino(1, board)
+            elif typeOriginal == 2:  # original block in Quadrant 2 (top-left)
+                b1 = (mid_x + 1, mid_y + 1)  # Quadrant 1: center tromino covers
+                b2 = block  # Quadrant 2: original block
+                b3 = (mid_x, mid_y)  # Quadrant 3: center tromino covers
+                b4 = (mid_x + 1, mid_y)  # Quadrant 4: center tromino covers
 
-            #recursion for type 1 problem
-            if board.get_boundary() == (1, 2, 1, 2):  # if we have 2x2
-                draw.draw_one_tromino(2, board)
-                return
-            else:
-                 self.solve(block, q1) #recursive call
-        elif block in q2:
-            draw.draw_one_tromino(2, board)
-            if board.get_boundary() == (1, 2, 1, 2):  # if we have 2x2
-                draw.draw_one_tromino(2, board)
-                return
-            else:
-                self.solve(block, q1)  # recursive call
+            elif typeOriginal == 3:  # original block in Quadrant 3 (bottom-left)
+                b1 = (mid_x + 1, mid_y + 1)  # Quadrant 1: center tromino covers
+                b2 = (mid_x, mid_y + 1)  # Quadrant 2: center tromino covers
+                b3 = block  # Quadrant 3: original block
+                b4 = (mid_x + 1, mid_y)  # Quadrant 4: center tromino covers
 
-        elif block in q3:
-            draw.draw_one_tromino(3, board)
-        elif block in q4:
-            draw.draw_one_tromino(4, board)
-        # your code goes here:
+            elif typeOriginal == 4:  # original block in Quadrant 4 (bottom-right)
+                b1 = (mid_x + 1, mid_y + 1)  # Quadrant 1: center tromino covers
+                b2 = (mid_x, mid_y + 1)  # Quadrant 2: center tromino covers
+                b3 = (mid_x, mid_y)  # Quadrant 3: center tromino covers
+                b4 = block  # Quadrant 4: original block
 
-
-
-
-
+            self.solve(b1, q1)
+            self.solve(b2, q2)
+            self.solve(b3, q3)
+            self.solve(b4, q4)
 
     def get_tromino_type(self, block, board):
-        # return the type of the tromino you should draw based on the position of the block and the board.
-        left, right, bottom, top = board.get_boundary() 
-        # your code goes here:
+        left, right, bottom, top = board.get_boundary()
+        mid_x = (left + right) // 2
+        mid_y = (bottom + top) // 2
 
+        # Quadrant 1 (top-right)
+        if block[0] > mid_x and block[1] > mid_y:
+            return 1
+        # Quadrant 2 (top-left)
+        elif block[0] <= mid_x and block[1] > mid_y:
+            return 2
+        # Quadrant 3 (bottom-left)
+        elif block[0] <= mid_x and block[1] <= mid_y:
+            return 3
+        # Quadrant 4 (bottom-right)
+        elif block[0] > mid_x and block[1] <= mid_y:
+            return 4
 
 
 
