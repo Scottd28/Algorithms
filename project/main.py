f2896d78fc19f41a31ddf681ed650ebb93b93d25
Scottd28 <ar0613@westminsteru.edu>
2025-11-29 23:07:55 -0700

    added parameter to know which algorithm you are performing

--- project/main.py
+++ project/main.py
@@ -1,3 +1,5 @@
+import argparse
+
 from bridges.bridges import *
 import math
 from bridges.data_src_dependent import data_source
@@ -5,13 +7,16 @@ from bridges.graph_adj_list import GraphAdjList
 from bridges.data_src_dependent import *
 from bridges.dl_element import *
 from bridges.us_map import *
+from nbformat.sign import algorithms
 
 
 #how to use the USCities Dataset
-def main():
+def main(algorithm_type):
 
     # create the Bridges object, set credentials
     bridges = Bridges(306, "ScottR1" ,"1011084303546")
+
+
     # map info
     state_info = get_us_map_data()
     my_map = USMap(state_info)
@@ -22,11 +27,10 @@ def main():
     print(len(major_cities))
     city_graph = GraphAdjList()
 
-
     #vertices
     for city in major_cities:
 
-        city_graph.add_vertex(city.city + ", " + city.state , city.city + ", " + city.state + ", " + str(city.population) )
+        city_graph.add_vertex(city.city + ", " + city.state , city.city + ", " + city.state + ", " + str(city.population) ) #key is CITY, STATE
         node_in_map = city_graph.get_visualizer(city.city + ", " + city.state)
         #set longitude and latitude
         node_in_map.set_location(city.lon, city.lat)
@@ -42,13 +46,22 @@ def main():
             if c1 == c2: continue # dont connect the same cities
 
             city2 = major_cities[c2]
-            d = haversine(city1.lat, city1.lon, city2.lat, city2.lon)
-            dists.append((d, city2))
-        dists.sort(key=lambda x: x[0])
-        for d, city2 in dists[:n_of_neighbors]:
+            distance = haversine(city1.lat, city1.lon, city2.lat, city2.lon)
+            dists.append((distance, city2))
+        dists.sort()
+        for distance, city2 in dists[:n_of_neighbors]:
             key2 = f"{city2.city}, {city2.state}"
-            city_graph.add_edge(key1, key2,d)
+            city_graph.add_edge(key1, key2,distance)
             city_graph.get_link_visualizer(key1, key2).thickness = 1.0
+
+
+    #figure what algorithm do you need to do
+    if algorithm_type.lower() == "prims":
+        prims(major_cities)  # or pass city_graph if you want
+    elif algorithm_type.lower() == "kruskals":
+        kruskals(city_graph)
+    else:
+        print("Invalid method. Choose 'Prims' or 'Kruskals'.")
 
 
   # visualizes map
@@ -59,22 +72,44 @@ def main():
 
 #calculate distances between cities
 def haversine(lat1, lon1, lat2, lon2):
-    radius = 6371
+    radius = 6371 #radius around the earth in km
     l1 = math.radians(lat1)
     l2 = math.radians(lat2)
-    dphi = math.radians(lat2 - lat1)
-    dlambda = math.radians(lon2 - lon1)
+    dlatitude = math.radians(lat2 - lat1)
+    dlongitude = math.radians(lon2 - lon1)
 
-    a = math.sin(dphi / 2) ** 2 + math.cos(l1) * math.cos(l2) * math.sin(dlambda / 2) ** 2
+    a = math.sin(dlatitude / 2) ** 2 + math.cos(l1) * math.cos(l2) * math.sin(dlongitude / 2) ** 2
     return 2 * radius * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
-def prims():
+def prims(start):
     #COMPLETE
+    '''
+    Select any vertex in G as the root of the MST.
+    Choose the vertex v that is closest to the set of vertices already taken.
+    Add v to the MST.
+    Goto step 2 until all vertices in G are in the MST.
+    '''
+
     return None
 
 def kruskals():
     #COMPLETE
+    '''
+    The general strategy is to select edges in order of smallest weight
+    and accept an edge if it does not form a cycle.
+    '''
     return None
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='MST Major Cities In America')
+
+    parser.add_argument(
+        '-method',
+        dest='algorithm_type',
+        required=True,
+        type=str,
+        help='Kruskals, Prims'
+    )
+    args = parser.parse_args()
+
+    main(args.algorithm_type)
