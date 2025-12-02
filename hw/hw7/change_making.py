5efe33d523be2a9b6da716fa480d6a6984dd450a
Scottd28 <ar0613@westminsteru.edu>
2025-12-01 23:50:37 -0700

    hw 7 updated

--- hw/hw7/change_making.py
+++ hw/hw7/change_making.py
@@ -1,7 +1,26 @@
+'''
+Give change for amount n using the minimum number of coins
+of denominations d1 < d2 < . . . < dm, where d1=1 and
+assuming availability of unlimited
+quantities of coins for each of the m denominations.
 
-def change_making(d,n):
-    # Your code goes here:
-    pass
+For example, n = 6 and coin denominations 1, 3, and 4.
+ The answer is 2 because you can use two 3 to add up to 6.
+'''
+ways = {}
+def change_making(d, n):
+    global ways
+    if n in ways:
+        return ways[n]
+    else:
+        choices = []
+        for coin in d:
+            if n % coin == 0:
+                #find the best coin combo for n
+                print("here")
+                choices.append(n/coin)
+
+        return ways[n]
 
 
 
--- hw/hw7/climb_stair.py
+++ hw/hw7/climb_stair.py
@@ -1,7 +1,21 @@
-
+'''
+There are n steps in a stair.
+You can either climb 1 or 2 steps each time.
+Find how many different ways you can climb to the top?
+'''
+ways = {0: 1, 1: 1, 2:2}
 def climb_stair(n):
     # Your code goes here:
-    pass
+    global ways
+
+    if n in ways:
+        return ways[n]
+    else:
+        ways[n] = climb_stair(n-1)+climb_stair(n-2)
+        return ways[n]
+
+
+
 
 
 
